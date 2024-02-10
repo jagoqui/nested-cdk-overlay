bd8fe0f8f5dbca6afedec58e297bda1928e6cab4
Jaidiver Gómez Quintero <jaidiver.gomez@experimentality.co>
2024-02-10 12:42:54 -0500

    feat: create nested cdk overlay.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+  MaOverlayModule, maFadeAnimation, MaDirectivesModule, MaModalComponent, MaInputsModule, MaBasicObject
+} from 'mis-aliados-library';
+import {
+  CdkConnectedOverlay, CdkOverlayOrigin, CdkScrollable, ConnectedPosition, Overlay, ScrollStrategyOptions
+} from '@angular/cdk/overlay';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MaOverlayModule, CdkOverlayOrigin, CdkConnectedOverlay, MaDirectivesModule, MaInputsModule, ReactiveFormsModule, CdkScrollable],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [maFadeAnimation]
 })
-export class AppComponent {
-  title = 'nested-cdk-overlay';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('modal') modal!: MaModalComponent;
+  readonly connectedPositions: ConnectedPosition[] = [{
+    originX: 'end', originY: 'top', overlayX: 'end', overlayY: 'bottom', offsetY: -22
+  }];
+  canShowToolTip = false;
+  control: FormControl<MaBasicObject<number> | null> = new FormControl<MaBasicObject<number> | null>(null);
+  cities = [{
+    id: 1, name: 'item-1'
+  }, {
+    id: 1, name: 'item-2'
+  }]
+  private _overlay: Overlay = inject(Overlay);
+
+  get scrollStrategies(): ScrollStrategyOptions {
+    return this._overlay.scrollStrategies;
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.modal.open())
+  }
 }
